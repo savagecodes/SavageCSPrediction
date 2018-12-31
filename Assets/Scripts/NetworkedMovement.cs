57c0d1d3fd3286a033a143589d3677b60d8a1301
Sergio <sergio.gardeazabal@savagecodes.com>
2018-12-31 00:38:08 -0300

    Cache Rigidbody property

--- Assets/Scripts/NetworkedMovement.cs
+++ Assets/Scripts/NetworkedMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+[RequireComponent(typeof(Rigidbody))]
 public class NetworkedMovement : NetworkBehaviour {
 
     const short InputMessageReceived = 1002;
@@ -16,6 +17,8 @@ public class NetworkedMovement : NetworkBehaviour {
     public float player_jump_y_threshold;
 
     public GameObject smoothed_client_player;
+
+    Rigidbody _rigidbody;
 
     private float client_timer;
     private uint client_tick_number;
@@ -50,18 +53,20 @@ public class NetworkedMovement : NetworkBehaviour {
     // Use this for initialization
     void Start () {
 
-        this.client_timer = 0.0f;
-        this.client_tick_number = 0;
-        this.client_last_received_state_tick = 0;
-        this.client_state_buffer = new ClientState[c_client_buffer_size];
-        this.client_input_buffer = new Inputs[c_client_buffer_size];
-        this.client_state_msgs = new BinaryHeap<StateMessage>();
-        this.client_pos_error = Vector3.zero;
-        this.client_rot_error = Quaternion.identity;
+        _rigidbody = GetComponent<Rigidbody>();
 
-        this.server_tick_number = 0;
-        this.server_tick_accumulator = 0;
-        this.server_input_msgs = new BinaryHeap<InputMessage>();
+        client_timer = 0.0f;
+        client_tick_number = 0;
+        client_last_received_state_tick = 0;
+        client_state_buffer = new ClientState[c_client_buffer_size];
+        client_input_buffer = new Inputs[c_client_buffer_size];
+        client_state_msgs = new BinaryHeap<StateMessage>();
+        client_pos_error = Vector3.zero;
+        client_rot_error = Quaternion.identity;
+
+        server_tick_number = 0;
+        server_tick_accumulator = 0;
+        server_input_msgs = new BinaryHeap<InputMessage>();
 
         if (isServer)
         {
@@ -100,7 +105,7 @@ public class NetworkedMovement : NetworkBehaviour {
 
         server_input_msgs_IDs.Add(message.packetId);
 
-        this.server_input_msgs.Enqueue(new HeapElement<InputMessage>(message,message.packetId));
+        server_input_msgs.Enqueue(new HeapElement<InputMessage>(message,message.packetId));
     }
 
     void OnStateMessageReceived(NetworkMessage netMsg)
@@ -119,11 +124,10 @@ public class NetworkedMovement : NetworkBehaviour {
         float dt = Time.fixedDeltaTime;
         uint server_tick_number = this.server_tick_number;
         uint server_tick_accumulator = this.server_tick_accumulator;
-        Rigidbody server_rigidbody = GetComponent<Rigidbody>();
 
-        while (this.server_input_msgs.Count > 0 && CurrentTime >= this.server_input_msgs.Peek().Element.delivery_time)
+        while (server_input_msgs.Count > 0 && CurrentTime >= server_input_msgs.Peek().Element.delivery_time)
         {
-            InputMessage input_msg = this.server_input_msgs.Dequeue().Element;
+            InputMessage input_msg = server_input_msgs.Dequeue().Element;
 
             // message contains an array of inputs, calculate what tick the final one is
             uint max_tick = input_msg.start_tick_number + (uint)input_msg.inputs.Length - 1;
@@ -139,12 +143,12 @@ public class NetworkedMovement : NetworkBehaviour {
                 // run through all relevant inputs, and step player forward
                 for (int i = (int)start_i; i < input_msg.inputs.Length; ++i)
                 {
-                    this.PrePhysicsStep(server_rigidbody, input_msg.inputs[i]);
+                    PrePhysicsStep(_rigidbody, input_msg.inputs[i]);
                     Physics.Simulate(dt);
 
                     ++server_tick_number;
                     ++server_tick_accumulator;
-                    if (server_tick_accumulator >= this.server_snapshot_rate)
+                    if (server_tick_accumulator >= server_snapshot_rate)
                     {
                         server_tick_accumulator = 0;
                         
@@ -152,10 +156,10 @@ public class NetworkedMovement : NetworkBehaviour {
                      state_msg.packetId = serverPacketID;
                      state_msg.delivery_time = CurrentTime + server_input_msgs.Peek().Element.rtt / 2;
                      state_msg.tick_number = server_tick_number;
-                     state_msg.position = server_rigidbody.position;
-                     state_msg.rotation = server_rigidbody.rotation;
-                     state_msg.velocity = server_rigidbody.velocity;
-                     state_msg.angular_velocity = server_rigidbody.angularVelocity;
+                     state_msg.position = _rigidbody.position;
+                     state_msg.rotation = _rigidbody.rotation;
+                     state_msg.velocity = _rigidbody.velocity;
+                     state_msg.angular_velocity = _rigidbody.angularVelocity;
 
                      //SendMesageToClient
                      base.connectionToClient.Send(StateMessageReceived, state_msg);
@@ -165,13 +169,13 @@ public class NetworkedMovement : NetworkBehaviour {
                 }
 
 
-                this.smoothed_client_player.transform.position = server_rigidbody.position;
-                this.smoothed_client_player.transform.rotation = server_rigidbody.rotation;
+                smoothed_client_player.transform.position = _rigidbody.position;
+                smoothed_client_player.transform.rotation = _rigidbody.rotation;
             }
         }
 
-        this.server_tick_number = server_tick_number;
-        this.server_tick_accumulator = server_tick_accumulator;
+       this.server_tick_number = server_tick_number;
+       this.server_tick_accumulator = server_tick_accumulator;
     }
 
     void ClientUpdate()
@@ -179,7 +183,6 @@ public class NetworkedMovement : NetworkBehaviour {
 
         CurrentTime += Time.deltaTime;
 
-        Rigidbody client_rigidbody = GetComponent<Rigidbody>();
         float dt = Time.fixedDeltaTime;
         float client_timer = this.client_timer;
         uint client_tick_number = this.client_tick_number;
@@ -198,12 +201,12 @@ public class NetworkedMovement : NetworkBehaviour {
             inputs.left = Input.GetKey(KeyCode.A);
             inputs.right = Input.GetKey(KeyCode.D);
             inputs.jump = Input.GetKey(KeyCode.Space);
-            this.client_input_buffer[buffer_slot] = inputs;
+            client_input_buffer[buffer_slot] = inputs;
 
             // store state for this tick, then use current state + input to step simulation
-            this.ClientStoreCurrentStateAndStep(
-                ref this.client_state_buffer[buffer_slot],
-                client_rigidbody,
+            ClientStoreCurrentStateAndStep(
+                ref client_state_buffer[buffer_slot],
+                _rigidbody,
                 inputs,
                 dt);
        
@@ -212,12 +215,12 @@ public class NetworkedMovement : NetworkBehaviour {
             input_msg.packetId = clientPacketID;
             input_msg.delivery_time = CurrentTime + rtt / 2 ;
             input_msg.rtt = rtt;
-            input_msg.start_tick_number = this.client_send_redundant_inputs ? this.client_last_received_state_tick : client_tick_number;
+            input_msg.start_tick_number = client_send_redundant_inputs ? client_last_received_state_tick : client_tick_number;
             var inputs_List = new List<Inputs>();
 
             for (uint tick = input_msg.start_tick_number; tick <= client_tick_number; ++tick)
             {
-                inputs_List.Add(this.client_input_buffer[tick % c_client_buffer_size]);
+                inputs_List.Add(client_input_buffer[tick % c_client_buffer_size]);
             }
 
             input_msg.inputs = inputs_List.ToArray();
@@ -229,65 +232,64 @@ public class NetworkedMovement : NetworkBehaviour {
             ++client_tick_number;
         }
 
-       // Debug.Log(ClientHasStateMessage());
-
-        if (this.ClientHasStateMessage())
+        if (ClientHasStateMessage())
         {
-            StateMessage state_msg = this.client_state_msgs.Dequeue().Element;
-            while (this.ClientHasStateMessage()) // make sure if there are any newer state messages available, we use those instead
+            StateMessage state_msg = client_state_msgs.Dequeue().Element;
+            while (ClientHasStateMessage()) // make sure if there are any newer state messages available, we use those instead
             {
-                state_msg = this.client_state_msgs.Dequeue().Element;
+                state_msg = client_state_msgs.Dequeue().Element;
             }
 
-            this.client_last_received_state_tick = state_msg.tick_number;
+            client_last_received_state_tick = state_msg.tick_number;
 
-            this.proxy_player.transform.position = state_msg.position;
-            this.proxy_player.transform.rotation = state_msg.rotation;
+            proxy_player.transform.position = state_msg.position;
+            proxy_player.transform.rotation = state_msg.rotation;
 
-            if (this.client_enable_corrections)
+            if (client_enable_corrections)
             {
                 uint buffer_slot = state_msg.tick_number % c_client_buffer_size;
-                Vector3 position_error = state_msg.position - this.client_state_buffer[buffer_slot].position;
-                float rotation_error = 1.0f - Quaternion.Dot(state_msg.rotation, this.client_state_buffer[buffer_slot].rotation);
+                Vector3 position_error = state_msg.position - client_state_buffer[buffer_slot].position;
+                float rotation_error = 1.0f - Quaternion.Dot(state_msg.rotation, client_state_buffer[buffer_slot].rotation);
 
                 if (position_error.sqrMagnitude > 0.0000001f ||
                     rotation_error > 0.00001f)
                 {
                     Debug.Log("Correcting");
+
                     // capture the current predicted pos for smoothing
-                    Vector3 prev_pos = client_rigidbody.position + this.client_pos_error;
-                    Quaternion prev_rot = client_rigidbody.rotation * this.client_rot_error;
+                    Vector3 prev_pos = _rigidbody.position + client_pos_error;
+                    Quaternion prev_rot = _rigidbody.rotation * client_rot_error;
 
                     // rewind & replay
-                    client_rigidbody.position = state_msg.position;
-                    client_rigidbody.rotation = state_msg.rotation;
-                    client_rigidbody.velocity = state_msg.velocity;
-                    client_rigidbody.angularVelocity = state_msg.angular_velocity;
+                    _rigidbody.position = state_msg.position;
+                    _rigidbody.rotation = state_msg.rotation;
+                    _rigidbody.velocity = state_msg.velocity;
+                    _rigidbody.angularVelocity = state_msg.angular_velocity;
               
 
                     uint rewind_tick_number = state_msg.tick_number;
                     while (rewind_tick_number < client_tick_number)
                     {
                         buffer_slot = rewind_tick_number % c_client_buffer_size;
-                        this.ClientStoreCurrentStateAndStep(
-                            ref this.client_state_buffer[buffer_slot],
-                            client_rigidbody,
-                            this.client_input_buffer[buffer_slot],
+                        ClientStoreCurrentStateAndStep(
+                            ref client_state_buffer[buffer_slot],
+                            _rigidbody,
+                            client_input_buffer[buffer_slot],
                             dt);
 
                         ++rewind_tick_number;
                     }
 
-                    // if more than 2ms apart, just snap
-                    if ((prev_pos - client_rigidbody.position).sqrMagnitude >= 4.0f)
+                    // if more than 2mts apart, just snap
+                    if ((prev_pos - _rigidbody.position).sqrMagnitude >= 4.0f)
                     {
-                        this.client_pos_error = Vector3.zero;
-                        this.client_rot_error = Quaternion.identity;
+                        client_pos_error = Vector3.zero;
+                        client_rot_error = Quaternion.identity;
                     }
                     else
                     {
-                        this.client_pos_error = prev_pos - client_rigidbody.position;
-                        this.client_rot_error = Quaternion.Inverse(client_rigidbody.rotation) * prev_rot;
+                        client_pos_error = prev_pos - _rigidbody.position;
+                        client_rot_error = Quaternion.Inverse(_rigidbody.rotation) * prev_rot;
                     }
                 }
             }
@@ -296,22 +298,21 @@ public class NetworkedMovement : NetworkBehaviour {
         this.client_timer = client_timer;
         this.client_tick_number = client_tick_number;
 
-        if (this.client_correction_smoothing)
+        if (client_correction_smoothing)
         {
-            this.client_pos_error *= 0.9f;
-            this.client_rot_error = Quaternion.Slerp(this.client_rot_error, Quaternion.identity, 0.1f);
+            client_pos_error *= 0.9f;
+            client_rot_error = Quaternion.Slerp(client_rot_error, Quaternion.identity, 0.1f);
         }
         else
         {
-            this.client_pos_error = Vector3.zero;
-            this.client_rot_error = Quaternion.identity;
+            client_pos_error = Vector3.zero;
+            client_rot_error = Quaternion.identity;
         }
 
-        this.smoothed_client_player.transform.position = client_rigidbody.position + this.client_pos_error;
-        this.smoothed_client_player.transform.rotation = client_rigidbody.rotation * this.client_rot_error;
+        smoothed_client_player.transform.position = _rigidbody.position + client_pos_error;
+        smoothed_client_player.transform.rotation = _rigidbody.rotation * client_rot_error;
     }
 	
-	// Update is called once per frame
 	void Update () {
         if (isServer) ServerUpdate();
         else ClientUpdate();
@@ -319,38 +320,38 @@ public class NetworkedMovement : NetworkBehaviour {
 
     private void PrePhysicsStep(Rigidbody rigidbody, Inputs inputs)
     {
-        if (this.local_player_camera_transform != null)
+        if (local_player_camera_transform != null)
         {
             if (inputs.up)
             {
-                rigidbody.AddForce(this.local_player_camera_transform.forward * this.player_movement_impulse, ForceMode.Impulse);
+                rigidbody.AddForce(local_player_camera_transform.forward * player_movement_impulse, ForceMode.Impulse);
             }
             if (inputs.down)
             {
-                rigidbody.AddForce(-this.local_player_camera_transform.forward * this.player_movement_impulse, ForceMode.Impulse);
+                rigidbody.AddForce(-local_player_camera_transform.forward * player_movement_impulse, ForceMode.Impulse);
             }
             if (inputs.left)
             {
-                rigidbody.AddForce(-this.local_player_camera_transform.right * this.player_movement_impulse, ForceMode.Impulse);
+                rigidbody.AddForce(-local_player_camera_transform.right * player_movement_impulse, ForceMode.Impulse);
             }
             if (inputs.right)
             {
-                rigidbody.AddForce(this.local_player_camera_transform.right * this.player_movement_impulse, ForceMode.Impulse);
+                rigidbody.AddForce(local_player_camera_transform.right * player_movement_impulse, ForceMode.Impulse);
             }
-            if (rigidbody.transform.position.y <= this.player_jump_y_threshold && inputs.jump)
+            if (rigidbody.transform.position.y <= player_jump_y_threshold && inputs.jump)
             {
-                rigidbody.AddForce(this.local_player_camera_transform.up * this.player_movement_impulse, ForceMode.Impulse);
+                rigidbody.AddForce(local_player_camera_transform.up * player_movement_impulse, ForceMode.Impulse);
             }
         }
     }
 
     private bool ClientHasStateMessage()
     {
-        if (this.client_state_msgs.Peek() == null) return false;
+        if (client_state_msgs.Peek() == null) return false;
 
-       // Debug.Log(CurrentTime + " vs " + this.client_state_msgs.Peek().Element.delivery_time);
+       // Debug.Log(CurrentTime + " vs " + client_state_msgs.Peek().Element.delivery_time);
 
-        return this.client_state_msgs.Count > 0 && CurrentTime >= this.client_state_msgs.Peek().Element.delivery_time;
+        return client_state_msgs.Count > 0 && CurrentTime >= client_state_msgs.Peek().Element.delivery_time;
     }
 
     private void ClientStoreCurrentStateAndStep(ref ClientState current_state, Rigidbody rigidbody, Inputs inputs, float dt)
@@ -358,7 +359,7 @@ public class NetworkedMovement : NetworkBehaviour {
         current_state.position = rigidbody.position;
         current_state.rotation = rigidbody.rotation;
 
-        this.PrePhysicsStep(rigidbody, inputs);
+        PrePhysicsStep(rigidbody, inputs);
         Physics.Simulate(dt);
     }
 
