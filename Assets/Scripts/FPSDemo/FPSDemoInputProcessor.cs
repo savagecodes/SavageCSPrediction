5f41559c556dcc7937782fba8c186cfbdbe83c6c
Sergio Gardeazabal <sergio.gardeazabal@savagecodes.com>
2019-05-07 23:19:23 -0300

    Clean Up Project

--- Assets/Scripts/FPSDemo/FPSDemoInputProcessor.cs
+++ Assets/Scripts/FPSDemo/FPSDemoInputProcessor.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityStandardAssets.Characters.FirstPerson;
-
+﻿using UnityEngine;
 public class FPSDemoInputProcessor : InputProcessor
 {
     private RigidbodyFirstPersonController _FPSController;
